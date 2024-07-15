42c583a8c033d2fef934ace98e278422c5c02e59
svncmsva <sevincmusaeva01@gmail.com>
2024-07-15 17:36:23 +0400

    table changed

--- source/repos/A302TechBlogWebApp/WebUI/obj/Debug/net8.0/WebUI.AssemblyInfo.cs
+++ source/repos/A302TechBlogWebApp/WebUI/obj/Debug/net8.0/WebUI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64c05ffc1d5a5051997a5a963c110a2695c02828")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
